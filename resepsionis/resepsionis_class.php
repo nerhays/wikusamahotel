a64ad8861cdfba0793700f28e74495f548d2b0a8
AimanYoviar <68726545+AimanYoviar@users.noreply.github.com>
2023-03-09 14:44:02 +0700

    second

---
+++ resepsionis/resepsionis_class.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+Class Action {
+	private $db;
+
+	public function __construct() {
+		ob_start();
+   	include 'db_connect.php';
+    
+    $this->db = $conn;
+	}
+	function __destruct() {
+	    $this->db->close();
+	    ob_end_flush();
+	}
+
+	function login(){
+		extract($_POST);
+		$qry = $this->db->query("SELECT * FROM users where username = '".$username."' and password = '".$password."' ");
+		if($qry->num_rows > 0){
+			foreach ($qry->fetch_array() as $key => $value) {
+				if($key != 'passwors' && !is_numeric($key))
+					$_SESSION['login_'.$key] = $value;
+			}
+			if($_SESSION['login_type'] == 1)
+				return 1;
+			else
+				return 2;
+		}else{
+			return 3;
+		}
+	}
+	function logout(){
+		session_destroy();
+		foreach ($_SESSION as $key => $value) {
+			unset($_SESSION[$key]);
+		}
+		header("location:../login.php");
+	}
+
+	function save_user(){
+		extract($_POST);
+		$data = " name = '$name' ";
+		$data .= ", username = '$username' ";
+		$data .= ", password = '$password' ";
+		$data .= ", type = '$type' ";
+		if(empty($id)){
+			$save = $this->db->query("INSERT INTO users set ".$data);
+		}else{
+			$save = $this->db->query("UPDATE users set ".$data." where id = ".$id);
+		}
+		if($save){
+			return 1;
+		}
+	}
+
+
+
+
+	
+
+}
+
